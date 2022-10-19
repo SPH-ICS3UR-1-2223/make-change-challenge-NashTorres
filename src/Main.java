b97c0e90236bbc0a8b38ea1014d868fb69462e38
NashTorres <113381172+NashTorres@users.noreply.github.com>
2022-10-19 14:10:41 -0400

    MakeChangeChallengeProject

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,39 @@ public class Main {
 			System.out.println(num20+" x $20");
 			change = change - (num20*20);
 			
+			int num10 = (int)(change/10);
+			System.out.println(num10+" x $20");
+			change = change - (num10*10);
+			
+			int num5 = (int)(change/5);
+			System.out.println(num5+" x $5");
+			change = change - (num5*5);
+			
+			int num2 = (int)(change/2);
+			System.out.println(num2+" x $2");
+			change = change - (num2*2);
+			
+			int num1 = (int)(change/1);
+			System.out.println(num1+" x $1");
+			change = change - (num1*1);
+			
+			double num05 = (double)(change/0.5);
+			System.out.println(num05+" x $0.5");
+			change = change - (num05*0.5);
+			
+			double num025 = (double)(change/0.25);
+			System.out.println(num025+" x $0.25");
+			change = change - (num025*0.25);
+			
+			double num01 = (double)(change/0.1);
+			System.out.println(num01+" x $0.1");
+			change = change - (num01*0.1);
+			
+			double num005 = (double)(change/0.05);
+			System.out.println(num005+" x $0.05");
+			change = change - (num005*0.05);
+			
+			
 			
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 			
